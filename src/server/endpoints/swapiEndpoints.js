238e2cc2f16f45b3f45c4f872fec652adef8b36b
unknown <web.madriders@gmail.com>
2023-09-04 19:59:10 +0200

    feat (getgetWeightOnPlanetRandom): añadida funcionalidad al endpoint getWeightOnPlanetRandom y convertida funcionalida de getPeople y getPlanet a funciones

--- src/server/endpoints/swapiEndpoints.js
+++ src/server/endpoints/swapiEndpoints.js
@@ -6,8 +6,44 @@ const _isWookieeFormat = (req) => {
     return false;
 }
 
-
 const applySwapiEndpoints = (server, app) => {
+    const getPeople = async (id) => {
+        let [dbPeopleData] = await app.db.swPeople.findAll({
+            where: {
+                id
+            }
+        });
+        if (dbPeopleData)
+            return {
+                name: dbPeopleData.name,
+                mass: dbPeopleData.mass,
+                height: dbPeopleData.height,
+                homeworldName: dbPeopleData.homeworld_name,
+                homeworldId: dbPeopleData.homeworld_id,
+            };
+        else {
+            const apiPeopleData = await app.swapiFunctions.genericRequest(`https://swapi.dev/api/people/${id}`, 'GET', null, true);
+            const apiPlanetData = await app.swapiFunctions.genericRequest(apiPeopleData.homeworld, 'GET', null, true);
+            return {
+                name: apiPeopleData.name,
+                mass: apiPeopleData.mass,
+                height: apiPeopleData.height,
+                homeworldName: apiPlanetData.name,
+                homeworldId: `/planets/${apiPeopleData.homeworld}`,
+            };
+        }
+    }
+    const getPlanet = async (id) => {
+        const [dbPlanetData] = await app.db.swPlanet.findAll({
+            where: {
+                id
+            }
+        });
+        if (dbPlanetData)
+            return { name: dbPlanetData.name, gravity: dbPlanetData.gravity };
+        const dataSwapi = await app.swapiFunctions.genericRequest(`https://swapi.dev/api/planets/${id}`, 'GET', null, true);
+        return { name: dataSwapi.name, gravity: dataSwapi.gravity.split(" ")[0] };
+    }
 
     server.get('/hfswapi/test', async (req, res) => {
         const data = await app.swapiFunctions.genericRequest('https://swapi.dev/api/', 'GET', null, true);
@@ -17,31 +53,8 @@ const applySwapiEndpoints = (server, app) => {
     server.get('/hfswapi/getPeople/:id', async (req, res) => {
         const { id } = req.params
         try {
-            let [dbPeopleData] = await app.db.swPeople.findAll({
-                where: {
-                    id
-                }
-            });
-            if (dbPeopleData)
-                return res.status(200).send({
-                    name: dbPeopleData.name,
-                    mass: dbPeopleData.mass,
-                    height: dbPeopleData.height,
-                    homeworldName: dbPeopleData.homeworld_name,
-                    homeworldId: dbPeopleData.homeworld_id,
-                });
-            else {
-                const apiPeopleData = await app.swapiFunctions.genericRequest(`https://swapi.dev/api/people/${id}`, 'GET', null, true);
-                const apiPlanetData = await app.swapiFunctions.genericRequest(apiPeopleData.homeworld, 'GET', null, true);
-                return res.status(200).send({
-                    name: apiPeopleData.name,
-                    mass: apiPeopleData.mass,
-                    height: apiPeopleData.height,
-                    homeworldName: apiPlanetData.name,
-                    homeworldId: `/planets/${apiPeopleData.homeworld}`,
-                });
-            }
-
+            const getPeopleData = await getPeople(id)
+            return res.status(200).send(getPeopleData);
         }
         catch (err) {
             return res.status(500).send(err.message);
@@ -51,16 +64,8 @@ const applySwapiEndpoints = (server, app) => {
     server.get('/hfswapi/getPlanet/:id', async (req, res) => {
         const { id } = req.params
         try {
-            const [dbPlanetData] = await app.db.swPlanet.findAll({
-                where: {
-                    id
-                }
-            });
-            if (dbPlanetData)
-                return res.status(200).send({ name: dbPlanetData.name, gravity: dbPlanetData.gravity });
-            const dataSwapi = await app.swapiFunctions.genericRequest(`https://swapi.dev/api/planets/${id}`, 'GET', null, true);
-            return res.status(200).send({ name: dataSwapi.name, gravity: dataSwapi.gravity.split(" ")[0] });
-
+            const getPlanetData = await getPlanet(id)
+            return res.status(200).send(getPlanetData);
         }
         catch (err) {
             return res.status(500).send(err);
@@ -68,7 +73,22 @@ const applySwapiEndpoints = (server, app) => {
     });
 
     server.get('/hfswapi/getWeightOnPlanetRandom', async (req, res) => {
-        res.sendStatus(501);
+
+        function getRandomInt(max) {
+            return Math.floor(Math.random() * max);
+        }
+        try {
+            const peopleId = getRandomInt(83);
+            const planetId = getRandomInt(60);
+            const peopleData = await getPeople(peopleId)
+            const planetData = await getPlanet(planetId)
+            if (peopleData.homeworldName === planetData.name) return res.status(401).send("El planeta es el natal del personaje");
+            const pesoPersonaje = parseInt(planetData.gravity) * parseInt(peopleData.mass)
+            return res.status(200).send({ pesoPersonaje });
+        }
+        catch (err) {
+            return res.status(500).send(err);
+        }
     });
 
     server.get('/hfswapi/getLogs', async (req, res) => {
